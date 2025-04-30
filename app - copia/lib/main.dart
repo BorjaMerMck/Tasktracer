c81cc3d1d163ad4ebd52c9e5b6ca7b780a72d195
BorjaMerMck <borjacursos146@gmail.com>
2025-04-30 20:59:23 +0200

    feat: implementar soporte para modo oscuro en la aplicación

--- app - copia/lib/main.dart
+++ app - copia/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 import 'firebase_options.dart';
 import 'screens/welcome/welcome_screen.dart';
 import 'screens/home/home_screen.dart';
